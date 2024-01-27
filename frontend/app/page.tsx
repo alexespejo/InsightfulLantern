00d17c28042a90992cc55903aa2c343e6c95ec27
Sebastian Bastida Marin <122469079+developersbm@users.noreply.github.com>
2024-01-27 06:21:02 +0000

    LogIn FRONTEND AND STUFF

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 import { signOut, useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
@@ -18,19 +17,6 @@ export default function Home() {
       <button className='text-white' onClick={() => signOut()}>Logout</button>
     </div>
   )
-=======
-"use client";
-import { redirect } from "next/navigation";
-import { useSession } from "next-auth/react";
-export default function Home() {
- const session = useSession({
-  required: true,
-  onUnauthenticated() {
-   redirect("/signin");
-  },
- });
- return <div className="p-8">Doing someting</div>;
->>>>>>> d196690fc73f04235a7be691d74fa7fe46d2da9f
 }
 
 Home.requireAuth = true;
