80aacc129fc7e45e7bf3367e06b2124a47872fd3
Sebastian Bastida Marin <122469079+developersbm@users.noreply.github.com>
2024-01-27 11:21:23 +0000

    MOOOOOOOOOOOOOOOOOON

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,51 @@
-'use client';
-import { signOut, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
-import Navbar from './components/Navbar';
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import Navbar from "./components/Navbar";
+import CircularMenu from "./components/moon/moon";
 
 export default function Home() {
-  const session = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/signin');
-    },
-  });
-  return (
-    <div>
-      <Navbar />
-      <div className='text-white'>{session?.data?.user?.email }</div>
-      <button className='text-white' onClick={() => signOut()}>Logout</button>
-    </div>
-  )
+ const session = useSession({
+  required: true,
+  onUnauthenticated() {
+   redirect("/signin");
+  },
+ });
+ return (
+  <div>
+   <Navbar />
+   <div className="text-white">{session?.data?.user?.email}</div>
+   <button className="text-white" onClick={() => signOut()}>
+    Logout
+   </button>
+
+   <form action={``} method="POST" className="d-flex flex-column">
+    <label className="form-control w-full max-w-xs">
+     <div className="label">
+      <span className="label-text">Title</span>
+     </div>
+     <input
+      type="text"
+      name="title"
+      placeholder="Type here"
+      className="input input-bordered w-full max-w-xs"
+     />
+    </label>
+    <label className="form-control w-full max-w-xs">
+     <div className="label">
+      <span className="label-text">Content</span>
+     </div>
+     <input
+      type="content"
+      name="title"
+      placeholder="Type here"
+      className="input input-bordered w-full max-w-xs"
+     />
+    </label>
+   </form>
+   <CircularMenu />
+  </div>
+ );
 }
 
 Home.requireAuth = true;
