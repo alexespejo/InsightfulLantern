f960a9bab32857258271cc72a3ca56e1129e6221
Alex Espejo <aespejo102@gmail.com>
2024-01-27 15:26:42 -0800

    fixed errors

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,44 +1,64 @@
-'use client';
+"use client";
 import { signOut } from "next-auth/react";
 import React, { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <div className="navbar bg-base-100" 
-    style={{ 
-      position: "fixed",
-      zIndex: "1000", 
-    }}>
-      <div className="flex-1">
-        {/* Use an anchor tag to wrap the image */}
-        <a href="/profile" className="btn btn-ghost text-xl">
-          <img className="mx-auto h-12 w-auto" src="/assets/logo.png" alt="Logo" />
-        </a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-3">
-        <Link href="/"><li><a>Home</a></li></Link>
-        <Link href="/screens/category"><li><a>Category</a></li></Link>
-        <Link href="/screens/about"><li><a>About</a></li></Link>
-          {/* User dropdown */}
-          <li>
-            <details>
-              <summary>
-                  <img src="/assets/userprofile.png" alt="Profile" className="h-8 w-8 rounded-full" />
-              </summary>
-              {/* Dropdown menu */}
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li><a>Account</a></li>
-                <li><a>Settings</a></li>
-                <li><a style={{color:"red",}} onClick={() => signOut()}>Log Out</a></li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+ return (
+  <div
+   className="navbar bg-base-100"
+   style={{
+    position: "fixed",
+    zIndex: "1000",
+   }}
+  >
+   <div className="flex-1">
+    {/* Use an anchor tag to wrap the image */}
+    <a href="/profile" className="btn btn-ghost text-xl">
+     <img className="mx-auto h-12 w-auto" src="/assets/logo.png" alt="Logo" />
+    </a>
+   </div>
+   <div className="flex-none">
+    <ul className="menu menu-horizontal px-3">
+     <Link href="/">
+      <li>Home</li>
+     </Link>
+     <Link href="/screens/category">
+      <li>Category</li>
+     </Link>
+     <Link href="/screens/about">
+      <li></li>
+     </Link>
+     {/* User dropdown */}
+     <li>
+      <details>
+       <summary>
+        <img
+         src="/assets/userprofile.png"
+         alt="Profile"
+         className="h-8 w-8 rounded-full"
+        />
+       </summary>
+       {/* Dropdown menu */}
+       <ul className="p-2 bg-base-100 rounded-t-none">
+        <li>
+         <a>Account</a>
+        </li>
+        <li>
+         <a>Settings</a>
+        </li>
+        <li>
+         <a style={{ color: "red" }} onClick={() => signOut()}>
+          Log Out
+         </a>
+        </li>
+       </ul>
+      </details>
+     </li>
+    </ul>
+   </div>
+  </div>
+ );
 };
 
 export default Navbar;
