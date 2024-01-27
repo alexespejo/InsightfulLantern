f4b5b0609dbe66e2a7f5e15eba6e87a435f179c8
Sebastian Bastida Marin <122469079+developersbm@users.noreply.github.com>
2024-01-27 22:37:42 +0000

    Again...

---
+++ frontend/app/screens/about/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import Navbar from "@/app/components/Navbar";
+import React from "react";
+import CircularMenu from "@/app/components/Moon/moon";
+
+function About() {
+ return <div>
+    <Navbar />
+ </div>;
+}
+
+export default About;
