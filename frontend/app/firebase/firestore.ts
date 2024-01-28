05f39052d62a0ebed2375743c9bd68d7a823d20b
Alex Espejo <aespejo102@gmail.com>
2024-01-27 18:02:46 -0800

    fix createUser

--- frontend/app/firebase/firestore.ts
+++ frontend/app/firebase/firestore.ts
@@ -2,8 +2,8 @@ import { create } from "domain";
 import { db } from "../firebase";
 import { collection, addDoc, doc, setDoc } from "firebase/firestore";
 
-export async function createUser(uid: string, uEmail: string) {
- await setDoc(doc(db, "users", uid), {
-  email: uEmail,
+export async function createUser(u_uid: string, uEmail: string) {
+ await setDoc(doc(db, "users", uEmail), {
+  uid: u_uid,
  });
 }
