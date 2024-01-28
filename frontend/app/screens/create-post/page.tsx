09678d98fc9e984f3c109e054c1b6b98bf09c494
Alex Espejo <aespejo102@gmail.com>
2024-01-27 19:42:21 -0800

    loading data to lanterns

--- frontend/app/screens/create-post/page.tsx
+++ frontend/app/screens/create-post/page.tsx
@@ -1,12 +1,38 @@
-'use client';
+"use client";
 import Navbar from "@/app/components/Navbar";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPostData } from "../../firebase/firestore";
 
 function Postpage() {
- return <div>
-    <Navbar />
+ const [posts, setPosts] = useState([]);
+ useEffect(() => {
+  // Use an async function to fetch the data
+  const fetchData = async () => {
+   try {
+    const citiesData: any = await getPostData("Coding");
+    setPosts(citiesData);
+   } catch (error: any) {
+    console.error("Error fetching cities:", error.message);
+   }
+  };
 
- </div>;
+  // Call the async function
+  fetchData();
+ }, []);
+ return (
+  <div className="text-slate-300">
+   <Navbar />
+   <h1>{posts?.length}</h1>
+   {posts.map((e: any, index) => {
+    return (
+     <div key={index}>
+      <h1>{e?.data?.title}</h1>
+      <h1>{e?.data?.content}</h1>
+     </div>
+    );
+   })}
+  </div>
+ );
 }
 
 export default Postpage;
